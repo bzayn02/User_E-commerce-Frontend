465a2b2dcb403437fa4a7aa56180b606873a794c
bzay_n02 <nagarkotibezay02@gmail.com>
2021-11-12 12:13:56 +1100

    Project setup ready

---
+++ frontend/README.md
@@ -0,0 +1,6 @@
+# E-commerce User CMS
+This project is for the user panel of the e-commerce site.
+
+## How to run
+- Clone the project
+- Run `npm install`
---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import {Button} from 'react-bootstrap'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Starting User CMS
+    </div>
+  );
+}
+
+export default App;
