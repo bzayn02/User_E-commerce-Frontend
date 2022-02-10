fbe824c1fd7128caabc32a93b502e1e7a7abaaeb
bzay_n02 <nagarkotibezay02@gmail.com>
2022-02-11 00:40:40 +1100

    header and footer done dashboard

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -1,5 +1,64 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
+const categories = [
+  'Mobile',
+  'TVs',
+  'Laptops',
+  'Computers',
+  'Camera',
+  'Wearables',
+  'Audio & Home Theatres',
+];
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <hr />
+      <Container>
+        {' '}
+        <Row>
+          <Col md="4">
+            {' '}
+            <div>
+              <span>Quick links</span>
+              <ul>
+                {categories.map((category, i) => (
+                  <li>{category}</li>
+                ))}
+              </ul>
+            </div>
+          </Col>
+          <Col md="4">
+            {' '}
+            <div>
+              <span>Quick links</span>
+              <ul>
+                {categories.map((category, i) => (
+                  <li>{category}</li>
+                ))}
+              </ul>
+            </div>
+          </Col>
+          <Col md="4">
+            {' '}
+            <div>
+              <span>Our mission</span>
+              <p>Quality products, sustainability, premium products.</p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <hr />
+      <Container>
+        <div className="text-center">
+          {' '}
+          <span>
+            &copy; {new Date().getFullYear()}, dawn-online-store Developed by
+            Bijay{' '}
+          </span>
+        </div>
+      </Container>
+    </div>
+  );
 };
--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,5 +1,43 @@
 import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 export const Header = () => {
-  return <div>Header</div>;
+  const categories = [
+    'Mobile',
+    'TVs',
+    'Laptops',
+    'Computers',
+    'Camera',
+    'Wearables',
+    'Audio & Home Theatres',
+  ];
+  return (
+    <div>
+      <div className="text-center">Free shipping available on all orders!</div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">DAWN</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#search">
+                <i class="fa-solid fa-magnifying-glass"></i>
+              </Nav.Link>
+              <Nav.Link href="#user">
+                <i class="fa-solid fa-user"></i>
+              </Nav.Link>
+              <Nav.Link href="#user">
+                <i class="fa-solid fa-bag-shopping"></i>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div className="d-flex justify-content-around flex-wrap">
+        {categories.map((category, i) => (
+          <Nav.Link>{category}</Nav.Link>
+        ))}
+      </div>
+    </div>
+  );
 };
